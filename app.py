899713340489576be44640c4c070d5d74c30eb70
Ronaldo Chan <ronaldopchan@outlook.com>
2024-11-10 05:00:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,11 +35,12 @@ def home():
             <body>
                 <center>
                    <h1><img src="https://getcomposer.org/img/logo-composer-transparent5.png"></img></h1>
+                   <h1>{{ app_message }}</h1>
                    <p>{{ app_name }} served from {{ hostname }}</p>
                 </center>
             </body>
         </html>
-    """, app_name=app_name, hostname=hostname)
+    """, app_name=app_name, app_title=app_title, app_message=app_message,hostname=hostname)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
