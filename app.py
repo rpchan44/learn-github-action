590cdb8458b8f68a06a093a31ff7dc99346d3f53
Ronaldo Chan <ronaldopchan@outlook.com>
2024-11-09 17:09:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def home():
             <body>
                 <center>
                     <h1><img src="https://getcomposer.org/img/logo-composer-transparent5.png"></img></h1>
+                    <h1>{{ app_message }}</h1>
                     <p>{{ app_name }} served from {{ hostname }}</p>
                 </center>
             </body>
