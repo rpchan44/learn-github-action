08cff98d5ac15ae7fcf873f67852391c998d3fd0
ronaldopchan@outlook.com <ronaldopchan@outlook.com>
2024-11-11 10:17:14 +0800

    pull data from mysql

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ COPY app.py /app/app.py
 
 # Install any needed dependencies
 RUN pip install --no-cache-dir -r requirements.txt
-
+RUN pip install mysql-connector-python
 EXPOSE 80
 
 # Set environment variable based on the properties file
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # app.py
 from flask import Flask, render_template_string
+import mysql.connector
+
 import os
 import socket  # Import socket library to get the hostname
 
@@ -16,6 +18,12 @@ def load_config(config_file):
 # Initialize Flask app
 app = Flask(__name__)
 
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'dev-mysql-external'
+app.config['MYSQL_USER'] = 'demo'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'demo'
+
 # Load configuration from config.properties (optional)
 config_path = os.getenv('CONFIG_FILE_PATH', '/app/config.properties')
 config = load_config(config_path)
@@ -23,24 +31,42 @@ app_name = config.get('app_name', 'app_name')
 app_title = config.get('app_title', 'app_title')
 app_message = config.get('app_message', 'app_message')
 
+
 # Get the hostname of the container
 hostname = socket.gethostname()
+
+def get_db_connection():
+        return mysql.connector.connect(
+            host=app.config['MYSQL_HOST'],
+            user=app.config['MYSQL_USER'],
+            password=app.config['MYSQL_PASSWORD'],
+            database=app.config['MYSQL_DB']
+        )
 
 @app.route('/')
 def home():
     # Render a simple HTML page with the app name and hostname of the container
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM demo")
+    data = cursor.fetchall()  # Get all rows
+    cursor.close()
+    connection.close()
+
     return render_template_string("""
         <html>
             <head><title>{{ app_title }}</title></head>
             <body>
                 <center>
                    <h1><img src="https://getcomposer.org/img/logo-composer-transparent5.png"></img></h1>
-                   <h1>{{ app_message }}</h1>
+	        {% for row in data %}
+                   <p>{{ row.message }}</p>
+                {% endfor %}
                    <p>{{ app_name }} served from {{ hostname }}</p>
                 </center>
             </body>
         </html>
-    """, app_name=app_name, app_title=app_title, app_message=app_message,hostname=hostname)
+    """, app_name=app_name, app_title=app_title, app_message=app_message,hostname=hostname,data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
