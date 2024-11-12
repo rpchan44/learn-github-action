7ec74dda2ad44b0374e440e15a89dc524d8f66c1
ronaldopchan@outlook.com <ronaldopchan@outlook.com>
2024-11-13 05:44:11 +0800

    specify template path

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def home():
     connection.close()
 
     return render_template(
-        'index.html',
+        'templates/index.html',
         app_dbname=app_dbname,
         app_name=app_name,
         app_title=app_title,
