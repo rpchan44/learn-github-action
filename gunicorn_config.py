11dd4e7d1b6307c71dd917dedf9bbcd96ac083c3
Ronaldo Chan <ronaldopchan@outlook.com>
2024-11-26 19:56:08 +0800

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,17 +1,3 @@
-# Copyright The OpenTelemetry Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
@@ -47,6 +33,6 @@ def post_fork(server, worker):
     # This uses insecure connection for the purpose of example. Please see the
     # OTLP Exporter documentation for other options.
     span_processor = BatchSpanProcessor(
-        OTLPSpanExporter(endpoint="collector.linkerd-jaeger.svc.cluster.local:4317", insecure=True)
+        OTLPSpanExporter(endpoint="collector.linkerd-jaeger.svc.cluster.local:55678", insecure=True)
     )
     trace.get_tracer_provider().add_span_processor(span_processor)
