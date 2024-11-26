749f86bb9cf25a120aed913e024d33a1d2c4cdcb
ronaldopchan@outlook.com <ronaldopchan@outlook.com>
2024-11-27 01:17:21 +0800

    make a dedicated route for probe

--- app.py
+++ app.py
@@ -93,6 +93,10 @@ def home():
         hostname=hostname,
         data=data)
 
+@app.route('/health')
+def probe:
+    return 'Ok', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
